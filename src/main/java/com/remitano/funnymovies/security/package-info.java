e7bd90f15fa6dabf4dd1b79c116ff43be72d7293
npthinh93 <npthinh93@gmail.com>
2022-08-17 23:11:23 +0700

    first commit

---
+++ src/main/java/com/remitano/funnymovies/security/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Spring Security configuration.
+ */
+package com.remitano.funnymovies.security;
---
+++ src/main/java/com/remitano/funnymovies/web/rest/vm/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * View Models used by Spring MVC REST controllers.
+ */
+package com.remitano.funnymovies.web.rest.vm;
